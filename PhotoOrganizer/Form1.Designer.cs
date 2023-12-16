1a899a94e4947eedc8f4f73b7d248cc619ae3ad8
airto <airto@DESKTOP-UDQJFDA>
2023-12-16 16:01:22 -0300

    Add project files.

---
+++ PhotoOrganizer/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PhotoOrganizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bt_execute = new System.Windows.Forms.Button();
+            this.countlbl = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(173, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source folder";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(227, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output Folder";
+            // 
+            // bt_execute
+            // 
+            this.bt_execute.Location = new System.Drawing.Point(407, 35);
+            this.bt_execute.Name = "bt_execute";
+            this.bt_execute.Size = new System.Drawing.Size(103, 23);
+            this.bt_execute.TabIndex = 4;
+            this.bt_execute.Text = "Execute";
+            this.bt_execute.UseVisualStyleBackColor = true;
+            this.bt_execute.Click += new System.EventHandler(this.bt_execute_Click);
+            // 
+            // countlbl
+            // 
+            this.countlbl.AutoSize = true;
+            this.countlbl.Location = new System.Drawing.Point(13, 402);
+            this.countlbl.Name = "countlbl";
+            this.countlbl.Size = new System.Drawing.Size(24, 13);
+            this.countlbl.TabIndex = 5;
+            this.countlbl.Text = "0/0";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 418);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.countlbl);
+            this.Controls.Add(this.bt_execute);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bt_execute;
+        private System.Windows.Forms.Label countlbl;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ PhotoOrganizer/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoOrganizer
+{
+    public partial class Form1 : Form
+    {
+        string sourceFolder = "H:\\OldOneDrive\\Pictures\\Câmera";
+        string outputFolder = "G:\\OutputImageOrganizer";
+        HashSet<string> hashSet = new HashSet<string>();
+        int totalFiles = 0;
+        int totalProcessed = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //open folder dialog to select a folder and set to sourceFolder string
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                sourceFolder = folderBrowserDialog1.SelectedPath;
+                textBox1.Text = sourceFolder;
+            }
+        }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                outputFolder = folderBrowserDialog1.SelectedPath;
+                textBox2.Text = outputFolder;
+            }
+        }
+
+        private async void bt_execute_Click(object sender, EventArgs e)
+        {
+
+            await Task.Run(() =>
+            {
+                //load hasehsFiles.txt
+                string hashesFiles = LoadFile(outputFolder + "\\hashesFiles.txt");
+                string[] hashes = hashesFiles.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+                //load copiedFiles.txt
+                string copiedFiles = LoadFile(outputFolder + "\\copiedFiles.txt");
+                string[] copied = copiedFiles.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+                //load ignoredFiles.txt
+                string ignoredFiles = LoadFile(outputFolder + "\\ignoredFiles.txt");
+                string[] ignored = ignoredFiles.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+
+                //check if sourceFolder and outputFolder are set
+                if (sourceFolder == null || outputFolder == null)
+                {
+                    MessageBox.Show("Please select a source folder and an output folder.");
+                }
+                else
+                {
+                    //get all files in sourceFolder
+                    string[] files = System.IO.Directory.GetFiles(sourceFolder, "*.*", System.IO.SearchOption.AllDirectories);
+
+                        totalFiles = files.Length;
+                        totalProcessed = 0;
+
+                    this.Invoke(new Action(() =>
+                    {
+                        countlbl.Text = totalProcessed.ToString() + " / " + totalFiles.ToString();
+                        progressBar1.Maximum = totalFiles;
+                        progressBar1.Value = 0;
+                    }));
+
+                    //loop through all files
+                    foreach (string file in files)
+                    {
+                        try
+                        {
+                            //check if file is in ignoredFiles
+                            if (ignored.Contains(file))
+                            {
+                                continue;
+                            }
+
+                            //check if file is in copiedFiles
+                            if (copied.Contains(file))
+                            {
+                                continue;
+                            }
+
+                            //get file extension
+                            string extension = System.IO.Path.GetExtension(file);
+
+                            //check if file is a jpg
+                            //get file hash
+                            string hash = GetFileHash(file);
+                            if (hash != null)
+                            {
+                                //check if file is in hashesFiles
+                                if (hashes.Contains(hash))
+                                {
+                                    continue;
+                                }
+
+                                //check if hash is already in hashSet
+                                if (!hashSet.Contains(hash))
+                                {
+                                    //add hash to hashSet
+                                    hashSet.Add(hash);
+                                    WriteToLog(hash, "hashesFiles");
+
+                                    //get date taken from file
+                                    DateTime dateTaken = GetDateTakenFromImage(file);
+
+                                    //create folder in outputFolder with year and month
+                                    string year = dateTaken.Year.ToString();
+                                    string month = dateTaken.Month.ToString();
+                                    string folderName = year + "\\" + month;
+                                    string folderPath = outputFolder + "\\" + folderName;
+                                    System.IO.Directory.CreateDirectory(folderPath);
+
+                                    //copy file to folder
+                                    string fileName = System.IO.Path.GetFileName(file);
+                                    string destFile = System.IO.Path.Combine(folderPath, fileName);
+                                    System.IO.File.Copy(file, destFile, true);
+                                    WriteToLog(folderPath + "\\" + fileName, "copiedFiles");
+                                }
+                                else
+                                {
+                                    WriteToLog($"DUPLICATE {file}", "duplicatedFiles");
+                                }
+                            }
+                            else
+                            {
+                                WriteToLog($"HASHFAIL {file}", "failFiles");
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            WriteToLog($"FAIL {file}", "failFiles");
+                        }
+
+                        totalProcessed++;
+                        this.Invoke(new Action(() => {
+                            countlbl.Text = totalProcessed.ToString() + " / " + totalFiles.ToString();
+                            progressBar1.Value = totalProcessed;
+                        }));
+                    }
+                }
+            });
+
+            MessageBox.Show("Done!");
+        }
+
+        public string LoadFile(string fileName)
+        {
+            //load file
+            if (System.IO.File.Exists(fileName))
+            {
+                string text = System.IO.File.ReadAllText(fileName);
+                return text;
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        public void WriteToLog(string text, string fileName)
+        {
+            //write to log
+            string logFile = outputFolder + "\\" + fileName + ".txt";
+            System.IO.File.AppendAllText(logFile, text + Environment.NewLine);
+        }
+
+
+        private string GetFileHash(string file)
+        {
+            //get file hash
+            string hash = "";
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                using (var stream = System.IO.File.OpenRead(file))
+                {
+                    hash = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+                }
+            }
+            return hash;
+        }
+
+        private DateTime GetDateTakenFromImage(string file)
+        {
+            try
+            {
+                //get date taken from image
+                System.Drawing.Imaging.PropertyItem propItem = null;
+                using (var fs = new System.IO.FileStream(file, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+                {
+                    var myImage = System.Drawing.Image.FromStream(fs, false, false);
+                    propItem = myImage.GetPropertyItem(36867);
+                }
+                string dateTaken = System.Text.Encoding.UTF8.GetString(propItem.Value).Trim();
+                string year = dateTaken.Substring(0, 4);
+                string month = dateTaken.Substring(5, 2);
+                string day = dateTaken.Substring(8, 2);
+                DateTime date = new DateTime(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day));
+                return date;
+            }
+            catch (Exception)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                return fileInfo.CreationTime;
+            }
+        }
+    }
+}
